d619aa798d6c8adc7485669c69b62a142611201d
Carlo Villarruz <gvillarruz@ryerson.ca>
2022-04-05 21:36:00 -0400

    Implement log in

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -9,19 +10,47 @@ import { MessageService } from 'primeng/api';
   providers: [MessageService],
 })
 export class SignInComponent implements OnInit {
-  username;
+  email;
   password;
 
   constructor(
     public loginService: LoginService,
     private router: Router,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {}
 
   signIn() {
-    this.loginService.loggedIn.next(true);
-    this.router.navigate(['/dashboard']);
+    if (this.email == null || this.password == null) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'All fields are required',
+      });
+    } else {
+      this.http
+        .post('https://cps-888-study-budies-ueaae.ondigitalocean.app/login', {
+          username: this.email,
+          password: this.password,
+        })
+        .subscribe((res: any) => {
+          console.log(res);
+
+          if (res.message == false) {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: 'Invalid username or password',
+            });
+          } else {
+            localStorage.setItem('token', res.token);
+            this.loginService.loggedIn.next(true);
+
+            this.router.navigate(['/dashboard']);
+          }
+        });
+    }
   }
 }
