215498c285ee88102c66ec7958a2bfec13209338
Mehali <mehali.modi@gmail.com>
2022-04-11 10:38:21 -0400

    fixed typos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
+
+  contactTodayButton() {
+    this.router.navigate(['/contact']);
+  }
+
+  registerTodayButton() {
+    this.router.navigate(['/student-signup']);
+  }
+
+  servicesOfferedButton() {
+    this.router.navigate(['/services']);
+  }
+
+
 
 }
